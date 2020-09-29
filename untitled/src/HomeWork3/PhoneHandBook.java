ff84e47e0878afc0d93878979b02ffbb449595f7
SergeyFedorov <Avel8985@gmail.com>
2020-09-29 21:20:31 +0300

    Lesson3 - Начал работать - Мастер- Комит и пуш

--- untitled/src/HomeWork3/PhoneHandBook.java
+++ untitled/src/HomeWork3/PhoneHandBook.java
@@ -9,36 +9,42 @@ import java.util.Map;
 
 public class PhoneHandBook {
 
-  private HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+    private HashMap<String, HashSet<Integer>> phoneBook = new HashMap<>();
 
-  public void add(String name, Integer number) {
-    try {
-      this.phoneBook.get(name).add(number);
-    } catch (NullPointerException e) {
-      ArrayList<Integer> arrayList = new ArrayList<>();
-      arrayList.add(number);
-      this.phoneBook.put(name, arrayList);
+    public void add(String name, Integer number) {
+        try {
+            this.phoneBook.get(name).add(number);
+        } catch (NullPointerException e) {
+            HashSet<Integer> hashSet = new HashSet<>();
+            hashSet.add(number);
+            this.phoneBook.put(name, hashSet);
+        }
     }
-  }
-  public void get(String name) {
-    try {
-      System.out.println(this.phoneBook.get(name));
-    } catch (NullPointerException e) {
-      System.out.println("Нет элементов в массиве");
+    public void get(String name) {
+        try {
+            System.out.println(this.phoneBook.get(name));
+        } catch (NullPointerException e) {
+            System.out.println("Нет элементов в массиве");
+        }
     }
-  }
 
 
 
-  public static void main(String[] args) {
-    PhoneHandBook phoneHandBook = new PhoneHandBook();
+    public static void main(String[] args) {
+        PhoneHandBook phoneHandBook = new PhoneHandBook();
 
-    phoneHandBook.add("Ssdas", 44555);
-    phoneHandBook.add("Ssdas", 44557);
-    System.out.println(phoneHandBook.phoneBook);
-    phoneHandBook.get("Ssdas");
+        phoneHandBook.add("Ssdas", 44555);
+        phoneHandBook.add("Ssdas", 44557);
+        phoneHandBook.add("Ssdas", 44557);
+        phoneHandBook.add("Ssdaasdas", 44557);
+        phoneHandBook.add("Ssdaafasf", 4454547);
+        phoneHandBook.add("Ssdaafasf", 4454547);
 
-  }
+
+        System.out.println(phoneHandBook.phoneBook);
+        phoneHandBook.get("Ssdas");
+
+    }
 
 
 }
